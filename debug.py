d03591a96e5ea4dbef9419184ff25fcb544a0575
AaronChen <lion860214@gmail.com>
2024-07-02 10:40:13 +0800

    "save"

--- debug.py
+++ debug.py
@@ -7,4 +7,4 @@ model_name = "staging.my_mlops_project.my_mlops_project-model"
 client = mlflow.tracking.MlflowClient()
 versions = client.get_latest_versions(model_name)
 for version in versions:
-    print(f"Version: {version.version}, Status: {version.current_stage}")
+    print(f"Version: {version.version}, Status: {version.current_stage}") 
